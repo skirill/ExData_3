3c45933fac346bab202d4be34a5c75b0e40fa138
skirill <kirill.senoshenko@gmail.com>
2014-12-19 23:23:05 +0600

    1 upd, 2

--- plot2.R
+++ plot2.R
@@ -5,14 +5,14 @@ checkReadRDS <- function(name)
     readRDS(name)
 }
 
-NEI <- checkReadRDS("summarySCC_PM25.rds")
+NEI <- subset(checkReadRDS("summarySCC_PM25.rds"), fips == "24510")
 #SCC <- checkReadRDS("Source_Classification_Code.rds")
 
 NEI.factored <- rowsum(NEI$Emissions, NEI$year)
 
 # Plotting
-png(file="plot1.png")
+png(file="plot2.png")
 par(bg = "transparent")
 barplot(NEI.factored[,1], names.arg = row.names(NEI.factored),
-        main = "PM 2.5 in the United States", xlab = "Year", ylab = "Emission")
+        main = "PM 2.5 in Baltimore City, Mariland, ", xlab = "Year", ylab = "Emission")
 dev.off()
